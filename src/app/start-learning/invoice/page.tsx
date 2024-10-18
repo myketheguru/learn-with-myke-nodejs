8397551e0f1e3e05c8fe7a8353967ad75122d26a
myketheguru <darkmyketheguru@gmail.com>
2024-10-18 10:04:21 +0100

    Handle page reload after interacting with paystack

--- src/app/start-learning/invoice/page.tsx
+++ src/app/start-learning/invoice/page.tsx
@@ -41,11 +41,19 @@ const InvoicePage = () => {
   const [loading, setLoading] = useState(true);
   const [paystackSuccess, setPaystackSuccess] = useState(false);
   const [paystackFailure, setPaystackFailure] = useState(false);
+  const [userHasEnrolled, setUserHasEnrolled] = useState(false);
+  const [cohortData, setCohortData] = useState<any>()
 
   const handleAccountSwitch = () => {
     if (typeof window !== "undefined") {
       localStorage.clear();
       router.push("/start-learning?triggerAuth=true");
+    }
+  };
+
+  const handleReload = () => {
+    if (typeof window !== "undefined") {
+      window.location.reload()
     }
   };
 
@@ -85,32 +93,39 @@ const InvoicePage = () => {
   };
 
   const getEnrollment = async (userId: string) => {
-    setLoading(true)
+    setLoading(true);
     try {
-        let res = await axios.get(`${baseUrl}/cohort/cohortDetails/${courseId}/${userId}`, {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-            },
-          });
+      let res = await axios.get(
+        `${baseUrl}/cohort/cohortDetails/${courseId}/${userId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+          },
+        }
+      );
 
-          const data = res.data.data;
-      console.log(data);
+      const data = res.data.data;
+      setCohortData(data)
+      if (data !== "No Cohort Found") {
+        setUserHasEnrolled(true);
+        setLoading(false);
+      }
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     const storedUser = localStorage.getItem("jwtUser");
     if (storedUser) {
       const parsedUser = JSON.parse(storedUser);
       setJwtUser(parsedUser);
-      getEnrollment(parsedUser?.id)
+          getEnrollment(parsedUser?.id);
     } else {
       // Get User
       handleAccountSwitch();
     }
-  }, []);
+  }, [userHasEnrolled]);
 
   useEffect(() => {
     async function fetchCourse() {
@@ -183,168 +198,186 @@ const InvoicePage = () => {
               </div>
             </div>
           </div>
-          <div className="grid place-items-center bg-g-to-grey rounded-[20px] shadow-lg overflow-hidden w-full">
-            <div className="bg-black p-6 w-[99.8%] h-[99.8%] rounded-[20px]">
-              <div className="flex justify-between items-center">
-                <div>
-                  <h1 className="text-xl font-semibold font-inter mb-1">
-                    Subscription summary
-                  </h1>
-
-                  <p className="text-xs opacity-50">
-                    Confirm details and proceed to make payment
-                  </p>
-                </div>
+          {userHasEnrolled ? (
+            <div className="bg-black p-3 w-[99.8%] h-[99%] rounded-[10px]">
+            <div className="flex gap-2 items-center">
+              <div className="rounded-full w-12 h-12 bg-slate-600">
+                
               </div>
 
-              <div className="mt-6">
-                <div className="flex flex-col gap-y-4">
-                  <div>
-                    <p className="font-medium text-lg capitalize">
-                      {jwtUser?.firstName} {jwtUser?.lastName}
-                    </p>
-                    <p className="text-xs opacity-50">Name</p>
-                  </div>
-                  <div>
-                    <p className="font-medium text-lg">
-                      {jwtUser?.email ?? "N/A"}
-                    </p>
-                    <p className="text-xs opacity-50">Email</p>
-                  </div>
+              <div>
+                <h1 className="text-base font-semibold font-inter capitalize">
+                  Looks like you've enrolled 🎉
+                </h1>
 
-                  <div>
-                    <p className="font-medium text-lg">
-                      ₦{formatNumber(course?.price || 0)}
-                    </p>
-                    <p className="text-xs opacity-50">Subscription fee</p>
-                  </div>
-
-                  <div>
-                    <p className="font-medium text-lg">
-                      {new Date().toLocaleDateString()}
-                    </p>
-                    <p className="text-xs opacity-50">Date</p>
-                  </div>
-
-                  {couponApplied && (
-                    <div>
-                      <p className="font-medium text-lg">
-                        ₦{formatNumber(couponData?.discountedAmount || 0)}
-                      </p>
-                      <p className="text-xs opacity-50">Discounted Price</p>
-                    </div>
-                  )}
-
-                  {!showCouponInput && (
-                    <div className="flex items-center gap-x-[6px]">
-                      <p className="text-xs">Got a coupon</p>
-                      <button
-                        type="button"
-                        onClick={() => setShowCouponInput(true)}
-                        className="text-[10px] text-[#3CC541] font-semibold w-[41px] h-[21px] text-center bg-[#008D53] rounded-[10px]"
-                      >
-                        Yes
-                      </button>
-                    </div>
-                  )}
-
-                  {showCouponInput && !couponData && (
-                    <>
-                      <div className="flex items-center gap-x-[6px]">
-                        <input
-                          className="outline-none w-[calc(100%_-_150px)] px-6 py-4 text-sm placeholder:text-white h-[52px] border rounded-[10px] bg-transparent text-white"
-                          placeholder="Enter coupon code"
-                          value={couponCode}
-                          onChange={(e) => {
-                            setCouponCode(e.target.value);
-                            setCouponError(null);
-                          }}
-                        />
-                        <button
-                          type="button"
-                          className="text-sm h-[52px] w-[156px] font-medium  text-center inline-flex items-center justify-center bg-[#008D53] rounded-[10px] disabled:opacity-50"
-                          disabled={couponCode?.length < 7}
-                          onClick={() => applyCoupon()}
-                        >
-                          {couponLoading ? (
-                            <svg
-                              className="animate-spin -ml-1 mr-3 h-5 w-5 text-emerald-200"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                            >
-                              <circle
-                                className="opacity-25"
-                                cx="12"
-                                cy="12"
-                                r="10"
-                                stroke="currentColor"
-                                stroke-width="4"
-                              ></circle>
-                              <path
-                                className="opacity-75"
-                                fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                              ></path>
-                            </svg>
-                          ) : (
-                            <span>Apply coupon</span>
-                          )}
-                        </button>
-                      </div>
-
-                      {couponError && (
-                        <p className="text-[10px] -mt-2 text-red-500">
-                          {couponError}
-                        </p>
-                      )}
-                    </>
-                  )}
-
-                  {couponData && couponData !== "Invalid Coupon Code" && (
-                    <div className="bg-emerald-900 inline-flex p-2 px-4 rounded-full self-start">
-                      <p className="text-xs">
-                        {couponData?.discountPercentage} discount applied ⚡
-                      </p>
-                    </div>
-                  )}
-                </div>
-
-                {/* Paystack button here */}
-                <PaystackButton
-                  user={{ ...jwtUser }}
-                  amount={courseAmount}
-                  courseId={courseId}
-                  setPaystackSuccess={setPaystackSuccess}
-                  setPaystackFailure={setPaystackFailure}
-                />
-
-                <div className="flex items-center text-[10px] gap-x-[4px]">
-                  <p>Got a question</p>
-
-                  <button
-                    type="button"
-                    className="text-[#008D53] font-semibold"
-                  >
-                    Ask Myke
-                  </button>
-                </div>
+                <p className="text-xs opacity-50">Please keep an eye on your mailbox for next steps.</p>
               </div>
             </div>
           </div>
+          ) : (
+            <div className="grid place-items-center bg-g-to-grey rounded-[20px] shadow-lg overflow-hidden w-full">
+              <div className="bg-black p-6 w-[99.8%] h-[99.8%] rounded-[20px]">
+                <div className="flex justify-between items-center">
+                  <div>
+                    <h1 className="text-xl font-semibold font-inter mb-1">
+                      Subscription summary
+                    </h1>
+
+                    <p className="text-xs opacity-50">
+                      Confirm details and proceed to make payment
+                    </p>
+                  </div>
+                </div>
+
+                <div className="mt-6">
+                  <div className="flex flex-col gap-y-4">
+                    <div>
+                      <p className="font-medium text-lg capitalize">
+                        {jwtUser?.firstName} {jwtUser?.lastName}
+                      </p>
+                      <p className="text-xs opacity-50">Name</p>
+                    </div>
+                    <div>
+                      <p className="font-medium text-lg">
+                        {jwtUser?.email ?? "N/A"}
+                      </p>
+                      <p className="text-xs opacity-50">Email</p>
+                    </div>
+
+                    <div>
+                      <p className="font-medium text-lg">
+                        ₦{formatNumber(course?.price || 0)}
+                      </p>
+                      <p className="text-xs opacity-50">Subscription fee</p>
+                    </div>
+
+                    <div>
+                      <p className="font-medium text-lg">
+                        {new Date().toLocaleDateString()}
+                      </p>
+                      <p className="text-xs opacity-50">Date</p>
+                    </div>
+
+                    {couponApplied && (
+                      <div>
+                        <p className="font-medium text-lg">
+                          ₦{formatNumber(couponData?.discountedAmount || 0)}
+                        </p>
+                        <p className="text-xs opacity-50">Discounted Price</p>
+                      </div>
+                    )}
+
+                    {!showCouponInput && (
+                      <div className="flex items-center gap-x-[6px]">
+                        <p className="text-xs">Got a coupon</p>
+                        <button
+                          type="button"
+                          onClick={() => setShowCouponInput(true)}
+                          className="text-[10px] text-[#3CC541] font-semibold w-[41px] h-[21px] text-center bg-[#008D53] rounded-[10px]"
+                        >
+                          Yes
+                        </button>
+                      </div>
+                    )}
+
+                    {showCouponInput && !couponData && (
+                      <>
+                        <div className="flex items-center gap-x-[6px]">
+                          <input
+                            className="outline-none w-[calc(100%_-_150px)] px-6 py-4 text-sm placeholder:text-white h-[52px] border rounded-[10px] bg-transparent text-white"
+                            placeholder="Enter coupon code"
+                            value={couponCode}
+                            onChange={(e) => {
+                              setCouponCode(e.target.value);
+                              setCouponError(null);
+                            }}
+                          />
+                          <button
+                            type="button"
+                            className="text-sm h-[52px] w-[156px] font-medium  text-center inline-flex items-center justify-center bg-[#008D53] rounded-[10px] disabled:opacity-50"
+                            disabled={couponCode?.length < 7}
+                            onClick={() => applyCoupon()}
+                          >
+                            {couponLoading ? (
+                              <svg
+                                className="animate-spin -ml-1 mr-3 h-5 w-5 text-emerald-200"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                              >
+                                <circle
+                                  className="opacity-25"
+                                  cx="12"
+                                  cy="12"
+                                  r="10"
+                                  stroke="currentColor"
+                                  stroke-width="4"
+                                ></circle>
+                                <path
+                                  className="opacity-75"
+                                  fill="currentColor"
+                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                ></path>
+                              </svg>
+                            ) : (
+                              <span>Apply coupon</span>
+                            )}
+                          </button>
+                        </div>
+
+                        {couponError && (
+                          <p className="text-[10px] -mt-2 text-red-500">
+                            {couponError}
+                          </p>
+                        )}
+                      </>
+                    )}
+
+                    {couponData && couponData !== "Invalid Coupon Code" && (
+                      <div className="bg-emerald-900 inline-flex p-2 px-4 rounded-full self-start">
+                        <p className="text-xs">
+                          {couponData?.discountPercentage} discount applied ⚡
+                        </p>
+                      </div>
+                    )}
+                  </div>
+
+                  {/* Paystack button here */}
+                  <PaystackButton
+                    user={{ ...jwtUser }}
+                    amount={courseAmount}
+                    courseId={courseId}
+                    setPaystackSuccess={setPaystackSuccess}
+                    setPaystackFailure={setPaystackFailure}
+                  />
+
+                  <div className="flex items-center text-[10px] gap-x-[4px]">
+                    <p>Got a question</p>
+
+                    <button
+                      type="button"
+                      className="text-[#008D53] font-semibold"
+                    >
+                      Ask Myke
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </>
       )}
 
       <AlertDialog
         open={paystackSuccess}
-        onOpenChange={(open) => setPaystackSuccess(open)}
+        onOpenChange={(open) => {setPaystackSuccess(open); open === false ? handleReload() : null}}
       >
         <AlertDialogContent className="bg-black border-emerald-500">
           <AlertDialogHeader>
             <AlertDialogTitle>Payment Successful 🎉</AlertDialogTitle>
             <AlertDialogDescription>
-              We have been notified of your payment. Please check your inbox for
-              any mails from us 🤩
+              We have been notified of your payment. Please check your inbox
+              (and spam) for any mails from us 🤩
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
@@ -355,7 +388,7 @@ const InvoicePage = () => {
 
       <AlertDialog
         open={paystackFailure}
-        onOpenChange={(open) => setPaystackFailure(open)}
+        onOpenChange={(open) => {setPaystackFailure(open); open === false ? handleReload() : null}}
       >
         <AlertDialogContent className="bg-black border border-emerald-500">
           <AlertDialogHeader>
